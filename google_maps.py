7baac3bc014b41a96af1845a3be33d3260cbe088
Rayhun <rayhunkhan27@gmail.com>
2021-12-22 23:51:58 +0600

    add requirements.txt

--- google_maps.py
+++ google_maps.py
@@ -7,7 +7,7 @@ longitude1 = 90.328700
 # Custom Icon
 custom_icon = folium.features.CustomIcon('icon.png', icon_size=(20, 20))
 #  Map
-map = folium.Map(location=[latitude, longitude], zoom_start=12)
+map = folium.Map(location=[latitude, longitude], zoom_start=12, control_scale=True)
 #  Marker
 folium.Marker(
     [latitude, longitude], popup="Bangladesh", icon=folium.Icon(color="red", icon="home"), tooltip="My Home"
@@ -22,5 +22,37 @@ folium.CircleMarker(
     color="green", fill_color="gray", fill_opacity=0.6, tooltip="My Home",
     fill=True, stroke=True, weight=2, opacity=0.8
 ).add_to(map)
+# add tiles to the map
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='Stamen Terrain', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='Stamen Toner', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='Stamen Watercolor', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='OpenStreetMap', attr='Map tiles by OpenStreetMap, under CC BY SA.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='CartoDB Positron', attr='Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='CartoDB DarkMatter', attr='Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='CartoDB PositronNoLabels', attr='Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='CartoDB PositronOnlyLabels', attr='Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='CartoDB DarkMatterNoLabels', attr='Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+map.add_child(folium.raster_layers.TileLayer(
+    tiles='CartoDB DarkMatterOnlyLabels', attr='Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+))
+folium.LayerControl().add_to(map)
 #  Save
 map.save("map.html")
