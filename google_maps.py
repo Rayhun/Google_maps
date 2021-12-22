6c30bcfcb090ef9a750764636501ada65af9cff3
Rayhun <rayhunkhan27@gmail.com>
2021-12-22 22:33:22 +0600

    add custome icone

--- google_maps.py
+++ google_maps.py
@@ -1,10 +1,19 @@
 import folium
 #  Bangladesh Location
-latitude = 23.8103
-longitude = 90.4125
+latitude = 24.098379
+longitude = 90.328712
+latitude1 = 24.098300
+longitude1 = 90.328700
+# Custom Icon
+custom_icon = folium.features.CustomIcon('icon.png', icon_size=(20, 20))
 #  Map
 map = folium.Map(location=[latitude, longitude], zoom_start=6)
 #  Marker
-folium.Marker([latitude, longitude], popup="Bangladesh").add_to(map)
+folium.Marker(
+    [latitude, longitude], popup="Bangladesh", icon=folium.Icon(color="red", icon="home"), tooltip="My Home"
+).add_to(map)
+folium.Marker(
+    [latitude1, longitude1], popup="Bangladesh", icon=custom_icon, tooltip="My Home"
+).add_to(map)
 #  Save
 map.save("map.html")
